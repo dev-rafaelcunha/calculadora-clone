1f8bd7427157bab802c09736d60403160fbfa6d4
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-05-22 16:57:05 -0300

    Iniciando a calculadora com o Metodo Construtor e Chamando o HTML via querySelector, (display, data e hora).

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,7 +3,19 @@ class CalcController
     constructor()
     {
         this._displayCalc = 0;
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize()
+    {
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
+
+        displayCalcEl.innerHTML = '4567';
+        dateEl.innerHTML = '22/05/2023';
+        timeEl.innerHTML = '16:53';
     }
 
     get displayCalc()
@@ -16,13 +28,13 @@ class CalcController
         this._displayCalc = valor;
     }
 
-    get dataAtual()
+    get currentDate()
     {
-        return this.dataAtual;
+        return this.currentDate;
     }
 
-    set dataAtual(valor)
+    set currentDate(valor)
     {
-        this.dataAtual = valor;
+        this.currentDate = valor;
     }
 }
