0b3586bc9b6716a8bfbdc66823f0c11ca772917d
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-05-22 16:20:44 -0300

    Desenvolvido metodo construtor, getters e setters para a classe CalcController.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,4 +1,28 @@
 class CalcController
 {
-    
+    constructor()
+    {
+        this._displayCalc = 0;
+        this._dataAtual;
+    }
+
+    get displayCalc()
+    {
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor)
+    {
+        this._displayCalc = valor;
+    }
+
+    get dataAtual()
+    {
+        return this.dataAtual;
+    }
+
+    set dataAtual(valor)
+    {
+        this.dataAtual = valor;
+    }
 }
