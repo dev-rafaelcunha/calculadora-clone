b275f397a32e3e987e18a5aa0fbd844bff9fa2b6
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-05-24 18:35:47 -0300

    Desenvolvido método para fazer a junção dos números, (arrays)

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -50,15 +50,37 @@ class CalcController
         this._operation[this._operation.length-1] = value;
     }
 
+    pushOperation(value)
+    {
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+        }
+    }
+
+    calc()
+    {
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(''));
+
+        this._operation = [result, last];
+    }
+
     isOperator(value)
     {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1 )
     }
 
+    // setLastNumberToDisplay()
+    // {
+
+    // }
+
     addOperation(value)
     {
-        console.log('A', isNaN(this.getLastOperation()));
-
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
@@ -67,20 +89,27 @@ class CalcController
 
             } else if (isNaN(value)) {
 
-                // Outra coisa
-                console.log(value);
+                console.log('Outra coisa', value);
 
             } else {
 
-                this._operation.push(value);
+                this.pushOperation(value);
             }
+
         } else {
 
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
-        }
+            if (this.isOperator(value)) {
 
-        console.log(this._operation);
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+            }
+        }
     }
 
     setError()
