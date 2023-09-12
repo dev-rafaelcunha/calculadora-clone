63eace2e8769e168cdfb97b15e155f0dd536677d
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-09-12 11:28:14 -0300

    Copiar e colar.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,6 +15,33 @@ class CalcController
         this.initKeyboard();
     }
 
+    pasteFromClipboard() 
+    {
+        document.addEventListener('paste', e=> {
+            
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+            console.log(text);
+        });
+    }
+
+    copyToClipboard()
+    {
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand('Copy');
+
+        input.remove();
+    }
+
     initialize()
     {
         this.setDisplayDateTime();
@@ -25,6 +52,7 @@ class CalcController
         }, 1000);
         
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
     }
 
     initKeyboard() 
@@ -70,6 +98,10 @@ class CalcController
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
                     break;
             }
         });
