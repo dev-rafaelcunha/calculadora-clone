2078a98c21fe1675e80d333cc9c5a9cd5c7707e7
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-09-12 11:37:58 -0300

    Trabalhando com Áudio.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController
 {
     constructor()
     {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this.lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -53,11 +55,35 @@ class CalcController
         
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=> {
+
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio()
+    {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio()
+    {
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() 
     {
         document.addEventListener('keyup', e=> {
+
+            this.playAudio();
 
             switch (e.key) {
                 
@@ -285,6 +311,8 @@ class CalcController
 
     execBtn(value)
     {
+        this.playAudio();
+
         switch (value) {
             case 'ac':
                     this.clearAll();
